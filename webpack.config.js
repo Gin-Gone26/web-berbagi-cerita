8e0041df4c3bf29618424fa8acbe60783acba3cd
Gin-Gone26 <ginanjarahmad54@gmail.com>
2025-09-16 17:30:35 +0700

    build: prepare for deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -9,7 +10,7 @@ module.exports = {
     chunkFilename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/',
+    publicPath: './', // 👈 penting untuk GitHub Pages (relative path)
   },
   devServer: {
     static: './dist',
@@ -18,13 +19,12 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
-      { test: /\.(png|jpg|jpeg|gif|svg)$/i, type: 'asset/resource' },
       {
-  test: /\.(woff|woff2|eot|ttf|otf)$/i,
-  type: 'asset/resource',
-},
-
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'], // 👈 ganti style-loader
+      },
+      { test: /\.(png|jpg|jpeg|gif|svg)$/i, type: 'asset/resource' },
+      { test: /\.(woff|woff2|eot|ttf|otf)$/i, type: 'asset/resource' },
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
@@ -34,16 +34,16 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({ template: './public/index.html' }),
-new CopyWebpackPlugin({
-  patterns: [
-    { from: 'public/manifest.json', to: '' },
-    { from: 'public/icon-192.png', to: '' },
-    { from: 'public/icon-512.png', to: '' },
-    { from: 'public/sw.js', to: '' },  
-    { from: 'public/idb.js', to: '' }, 
-  ],
-}),
-
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }), // 👈 generate file CSS
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public/manifest.json', to: '' },
+        { from: 'public/icon-192.png', to: '' },
+        { from: 'public/icon-512.png', to: '' },
+        { from: 'public/sw.js', to: '' },
+        { from: 'public/idb.js', to: '' },
+      ],
+    }),
   ],
   resolve: { extensions: ['.js'] },
 };
